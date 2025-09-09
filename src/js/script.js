998a611d31a303b297a8ff1583cc9b6b8649973b
orujinclothing <orujin.clothing@gmail.com>
2025-09-09 21:36:06 +0530

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,52 +1,40 @@
-// Header adjustment for <768px
+// ===== Navigation Toggle =====
 const toggle = document.querySelector(".toggle");
 const menu = document.querySelector(".nav-menu");
-function toggleMenu() {
-    if (menu.classList.contains("active")) {
-        menu.classList.remove("active");
-        // add hamburger icon
-        toggle.innerHTML = `<i class="fa fa-bars"></i>`;
-    }
-    else {
-        menu.classList.add("active");
-        // add X icon
-        toggle.innerHTML = `<i class="fa fa-times"></i>`;
-    }
-}
-toggle.addEventListener("click", toggleMenu, false);
+const toggleIcon = toggle.querySelector("i");
 
-// Account Page, hide and show elements when, and if required.
-var logForm = document.getElementById("loginForm");
-var regForm = document.getElementById("registerForm");
-var remLog = document.getElementById("removeLog");
-var remReg = document.getElementById("removeReg");
+toggle.addEventListener("click", () => {
+    menu.classList.toggle("active");
+    toggleIcon.classList.toggle("fa-bars");
+    toggleIcon.classList.toggle("fa-times");
+});
+
+// ===== Account Page (Login / Register) =====
+const logForm = document.getElementById("loginForm");
+const regForm = document.getElementById("registerForm");
+const remLog = document.getElementById("removeLog");
+const remReg = document.getElementById("removeReg");
 
 function login() {
-    remLog.style.display = "none";
-    remReg.style.display = "block";
     logForm.style.display = "flex";
     regForm.style.display = "none";
+    remLog.style.display = "none";
+    remReg.style.display = "block";
 }
 
 function register() {
-    remReg.style.display = "none";
-    remLog.style.display = "block";
-    regForm.style.display = "flex";
     logForm.style.display = "none";
+    regForm.style.display = "flex";
+    remLog.style.display = "block";
+    remReg.style.display = "none";
 }
 
-// Preview images for the same product slider
-var prodImg = document.getElementById("prodImg");
-var smallImg = document.getElementsByClassName("small-img");
-smallImg[0].onclick = function () {
-    prodImg.src = smallImg[0].src;
-}
-smallImg[1].onclick = function () {
-    prodImg.src = smallImg[1].src;
-}
-smallImg[2].onclick = function () {
-    prodImg.src = smallImg[2].src;
-}
-smallImg[3].onclick = function () {
-    prodImg.src = smallImg[3].src;
-}
+// ===== Product Image Preview =====
+const prodImg = document.getElementById("prodImg");
+const smallImgs = document.querySelectorAll(".small-img");
+
+smallImgs.forEach(img => {
+    img.addEventListener("click", () => {
+        prodImg.src = img.src;
+    });
+});
